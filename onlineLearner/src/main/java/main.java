1a1c7656cb930d8ad46c78d028da7d15119170ce
Ahmed <amohy2015@gmail.com>
2021-01-19 00:02:18 +0100

    adding a main method to test the output of the database (not necessary in the project only for testing )

---
+++ onlineLearner/src/main/java/main.java
@@ -0,0 +1,39 @@
+
+
+
+
+import java.sql.*;
+
+import de.unidue.inf.is.domain.User;
+import de.unidue.inf.is.stores.StoreException;
+import de.unidue.inf.is.utils.DBUtil;
+
+
+public class main {
+
+    private static Connection connection;
+
+    private boolean complete;
+public static void main (String[] args){
+
+    try {
+        connection = DBUtil.getExternalConnection() ;
+        connection.setAutoCommit(false);
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    try {
+        Statement statment = connection.createStatement();
+
+       String s = "select name from dbp155.kurs";
+      ResultSet rs =  statment.executeQuery(s);
+      while(rs.next()){
+          System.out.println(rs.getString("name"));
+      }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }finally {
+        System.out.println("Ya rab");
+    }
+}
+}
